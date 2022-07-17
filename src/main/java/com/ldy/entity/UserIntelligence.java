5446f957761f99a603d294aa672dfb20cf6025dc
liaoduoduo <386160844@qq.com>
2022-07-17 13:16:33 +0800

    user_intelligence表设计

--- src/main/java/com/ldy/entity/UserIntelligence.java
+++ src/main/java/com/ldy/entity/UserIntelligence.java
@@ -10,14 +10,17 @@ import java.time.LocalDateTime;
  * @Author ldy
  * @Date 2022/7/8 22:21
  * @ClassName UserIntelligence
- * @Description 用户拥有情报
+ * @Description 用户购买的情报
  * @Version v1.0
  */
 @Data
 public class UserIntelligence {
     private Long id;
     private Long userId;
+
+    //intelligence表主键
     private Long intelligenceId;
+    //intelligence表主键对应fileHash字段
     private String fileHash;
 
     @TableField(fill = FieldFill.INSERT)
